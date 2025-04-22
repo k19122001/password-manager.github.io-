416ac5dbd0b4c958dbe56c5c3b1b9763fb18f22d
k19122001 <141487769+k19122001@users.noreply.github.com>
2025-04-22 21:08:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://k19122001.github.io/password-manager.github.io-/",
+  base: "./",
 })
